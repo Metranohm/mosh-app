1469ac39d04c90ad3569bbe658c61a0010a9db62
Metranohm <andrewwink@gmail.com>
2022-12-21 16:44:11 -0600

    NavBar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import NavBar from "./Components/NavBar";
+import { ReactDOM } from "react";
+
 
 function App() {
   return (
     <div className="App">
+      <NavBar />
       <h1>Hello World</h1>
     </div>
   );
